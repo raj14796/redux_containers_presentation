8717d744407bad5a1454c668f13cd51d9c8738b4
Ravi Raj <raviraj14796@gmail.com>
2021-09-13 08:15:24 +0530

    design done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import HomeContainer from './containers/HomeContainer'
+import "./App.css"
 
 const App = () => {
     return (
         <div>
             App
+            <HomeContainer />
         </div>
     )
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import iphone12image from '../iphone12.jpeg'
+import cart_icon from '../cart_icon.png'
 
 const Home = () => {
     return (
         <div>
-            
+            <div className="add-to-cart">
+                <img src={cart_icon} alt="cart_icon"></img>
+            </div>
+            Home Component
+            <div className="cart-wrapper">
+                <div className="img-wrapper item">
+                    <img src={iphone12image} alt="iphone12image"></img>
+                </div>
+                <div className="text-wrapper item">
+                    <span>I-Phone 12</span><br />
+                    <span>Price : Rs 78000</span>
+                </div>
+                <div className="btn-wrapper item">
+                    <button>Add to Cart</button>
+                </div>
+            </div>
         </div>
     )
 }
